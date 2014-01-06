20d38badc8039c2a70cae74a7ad721fec7da28aa
Ben Emson <ben@emson.co.uk>
2014-01-06 23:10:57 +0000

    Added Jen's front page design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+//= require zepto.min
 //= require turbolinks
-//= require_tree .
+//= require utils
+//= require carousel
