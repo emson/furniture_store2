18ef9140d65485e9fb7fd0269a0756a5c7e4e2e1
Ben Emson <ben@emson.co.uk>
2014-01-29 22:51:57 +0000

    Fixed default seats setting

--- app/admin/furnishing.rb
+++ app/admin/furnishing.rb
@@ -1,39 +1,30 @@
 
-def default_select(attrib, default)
+def default_select(furnishing, attrib, default)
   return default if furnishing.nil?
   value = furnishing.send(attrib)
-  return default if value.nil? || value.to_s.empty?
+  return default if value.nil? || value.to_s.gsub(/[\s|\t]/,'').empty?
   value
+end
+
+def default_select_seats(furnishing, attrib, default)
+  # return default if furnishing.nil?
+  # value = furnishing.seats
+  # puts "=========== #{value}"
+  # return default if value.nil? || value.to_s.gsub(/[\s|\t]/,'').empty?
+  # value
+  value = furnishing.seats
+  puts "=========== #{value}"
+  value.to_s
 end
 
 ActiveAdmin.register Furnishing do
 
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  # permit_params(Furnishing.permitted)
-
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
   controller do
     def permitted_params
       params.permit!
     end
   end
 
-  # show do
-  #   h3 furnishing.name
-  #   div do
-  #     simple_format furnishing.description
-  #   end
-  # end
   show do |furnishing|
     attributes_table do
       row :id
@@ -57,9 +48,9 @@ end
 
 form do |f|
   f.inputs "Details" do
-    f.input :furnishing_type, :as => :select, :collection => options_for_select(%w{armchair chair sofa}, default_select(:furnishing_type, 'armchair'))
-    f.input :shape, :as => :select, :collection => options_for_select(%w{arun avon dart frome hamble lambourn otter parrett stour wylye}, default_select(:shape, 'arun'))
-    f.input :seats, :as => :select, :collection => options_for_select(%w{1 2 2.5 3}, default_select(:seats, '1'))
+    f.input :furnishing_type, :as => :select, :collection => options_for_select(%w{armchair chair sofa}, default_select(furnishing, :furnishing_type, 'armchair'))
+    f.input :shape, :as => :select, :collection => options_for_select(%w{arun avon dart frome hamble lambourn otter parrett stour wylye}, default_select(furnishing, :shape, 'arun'))
+    f.input :seats, :as => :select, :collection => options_for_select(%w{1.0 2.0 2.5 3.0}, default_select(furnishing, :seats, '1.0'))
     f.input :name
     f.input :description
     f.input :back
