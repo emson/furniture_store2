2182fac538079c67f10ab2735cd9c8e9cb09822c
Ben Emson <ben@emson.co.uk>
2016-03-07 11:25:17 +0000

    Add basic working random testimonials to main index view

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,8 +6,14 @@ class MainController < ApplicationController
   # carousel/IMG_2046.jpg
   # carousel/IMG_2071.jpg
   def index
-    @carousels = Carousel.order(:order => :asc)
+    @carousels    = Carousel.order(:order => :asc)
+    @testimonials = select_random(Testimonial, 2)
   end
 
+  private
+
+  def select_random(model, size=3)
+    model.all.shuffle[1, size]
+  end
 
 end
